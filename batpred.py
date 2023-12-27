2fd84f3ae787e516b08b2bde6bb16472619c7ed5
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2023-12-27 13:35:28 +0000

    Create batpred.py

---
+++ batpred.py
@@ -0,0 +1,24 @@
+"""
+Battery Prediction app
+see Readme for information
+"""
+# fmt off
+# pylint: disable=consider-using-f-string
+# pylint: disable=line-too-long
+# pylint: disable=attribute-defined-outside-init
+from datetime import datetime, timedelta
+import math
+import re
+import time
+import pytz
+import requests
+import copy
+import os
+import yaml
+
+def main():
+  print("Batpred startup python")
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())  # next section explains the use of sys.exit
