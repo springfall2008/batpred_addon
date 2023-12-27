6d26aeb0004001ab1bf1140c70b9a2b6475d32d9
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2023-12-27 18:46:54 +0000

    Update batpred.py

--- batpred.py
+++ batpred.py
@@ -15,9 +15,30 @@ import requests
 import copy
 import os
 import yaml
+import sys
+
+CORE_API = "http://supervisor/core/api"
+SUPERVISOR_TOKEN = os.environ.get('SUPERVISOR_TOKEN', '')
+
+def read_states():
+    """
+    Get inverter status
+    """
+    url = CORE_API + "/" + "states"
+    headers = {"Authorization" : "Bearer " + SUPERVISOR_TOKEN, "Content-Type" : "application/json"}
+    try:
+        r = requests.get(url, headers=headers)
+    except Exception as e:
+        print("ERROR: Exception raised {}".format(e))
+        r = None
+
+    if r and (r.status_code == 200):
+        json = r.json()
+        print("Got {}".format(json))
 
 def main():
   print("Batpred startup python")
+  read_states()
   return 0
 
 if __name__ == '__main__':
